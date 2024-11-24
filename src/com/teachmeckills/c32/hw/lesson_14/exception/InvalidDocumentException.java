4c0b3e04c2fc4a0f59f851154e6b52ffc3ecc0b6
Pr1 <karpovicvlad042@gmail.com>
2024-11-24 16:54:51 +0300

    Init commit

--- src/com/teachmeckills/c32/hw/lesson_14/exception/InvalidDocumentException.java
+++ src/com/teachmeckills/c32/hw/lesson_14/exception/InvalidDocumentException.java
@@ -1,6 +1,6 @@
 package com.teachmeckills.c32.hw.lesson_14.exception;
 
-// Кастомное исключение для невалидных документов
+
 public class InvalidDocumentException extends Exception {
     public InvalidDocumentException(String message) {
         super(message);
